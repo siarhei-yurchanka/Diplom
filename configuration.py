6b0c73274f46ffc5a07111d13e93bb3ecad00dc2
siarhei-yurchanka <siarhei.yurchanka1985@gmail.com>
2024-12-16 18:25:59 +0100

    Update configuration.py
    Удаление / в конце URL_SERVICE

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = "https://51f45b17-fd8d-49b1-8900-48b1c45601b9.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://51f45b17-fd8d-49b1-8900-48b1c45601b9.serverhub.praktikum-services.ru"
 
 # CREATE_ORDER хранит путь к API-методу для создания нового заказа.
 CREATE_ORDER = "/api/v1/orders"
